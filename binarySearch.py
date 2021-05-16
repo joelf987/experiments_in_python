025de6b1df08a3a324047cde4492eb41cc56c583
Joel Fernandes <joel.miguel.fernandes@gmail.com>
2021-05-16 18:51:25 +0100

    - removed parameter for input_list from bsearch

--- binarySearch.py
+++ binarySearch.py
@@ -1,7 +1,7 @@
-inputList = [5, 6, 8, 9, 15]
+input_list = [5, 6, 8, 9, 15]
 
 
-def bsearch(input_list, number_to_search, start_index=0, end_index=len(inputList) - 1):
+def bsearch(number_to_search, start_index=0, end_index=len(input_list) - 1):
     midpoint: int = (start_index + end_index) // 2
     # terminal condition 1: have we reached the end of the list?
     if start_index == len(input_list) - 1:
@@ -10,14 +10,14 @@ def bsearch(input_list, number_to_search, start_index=0, end_index=len(inputList
     if number_to_search_for == input_list[midpoint]:
         return midpoint
     elif number_to_search_for < input_list[midpoint]:
-        return bsearch(input_list, number_to_search, 0, midpoint - 1)
+        return bsearch(number_to_search, 0, midpoint - 1)
     else:
-        return bsearch(input_list, number_to_search, midpoint + 1, end_index)
+        return bsearch(number_to_search, midpoint + 1, end_index)
 
 
 number_to_search_for = int(input("What number are you looking for? "))
 
-index = bsearch(inputList, number_to_search_for)
+index = bsearch(number_to_search_for)
 if index == -1:
     print("Number ", number_to_search_for, " not found ")
 else:
